7baa789d685c0e4950119159da6981be81db4444
John Benjamin <john@rekonnect.io>
2025-12-13 01:08:51 +0100

    removed TODOs in template Day 04

---
+++ AdventOfCode2025/Days/Day04.cs
@@ -0,0 +1,16 @@
+namespace AdventOfCode2025.Days;
+
+public class Day04 : IDay
+{
+    public int DayNumber => 0;
+
+    public string SolvePart1(string input)
+    {
+        return "Not Solved";
+    }
+
+    public string SolvePart2(string input)
+    {
+        return "Not Solved";
+    }
+}
