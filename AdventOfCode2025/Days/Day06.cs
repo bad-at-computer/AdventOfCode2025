6b17f53a43dcf9f8867688195a061a4ad2c76674
johnbenjamin <jgbenjam@gmail.com>
2025-12-18 23:57:23 +0100

    Added Day 6 part 1

---
+++ AdventOfCode2025/Days/Day06.cs
@@ -0,0 +1,52 @@
+namespace AdventOfCode2025.Days;
+
+public class Day06 : IDay
+{
+    public int DayNumber => 6;
+
+    public string SolvePart1(string input)
+    {
+        var rows = input.Split('\n');
+        var height = rows.Length;
+        var mathArray = new string[height][];
+        var total = 0L;
+
+        for (var i = 0; i < height; i++)
+        {
+            mathArray[i] = rows[i].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        }
+        
+        
+        for (var i = 0; i < mathArray[0].Length; i++)
+        {
+            var currentTotal = 0L;
+
+            if (mathArray[height - 1][i].Equals("+"))
+            {
+                for (var j = 0; j < mathArray.Length - 1; j++)
+                {
+                    currentTotal += long.Parse(mathArray[j][i]);
+                }
+            }
+
+            if (mathArray[height - 1][i].Equals("*"))
+            {
+                currentTotal = 1;
+                for (var j = 0; j < mathArray.Length - 1; j++)
+                {
+                    currentTotal *= long.Parse(mathArray[j][i]);
+                }
+            }
+
+            total += currentTotal;
+            Console.WriteLine($"Adding line {i}: {currentTotal}");
+        }
+
+        return total.ToString();
+    }
+
+    public string SolvePart2(string input)
+    {
+        return "not implemented";
+    }
+}
--- AdventOfCode2025/Program.cs
+++ AdventOfCode2025/Program.cs
@@ -29,7 +29,7 @@ var days = new Dictionary<int, IDay>
     { 3, new Day03() },
     { 4, new Day04() },
     { 5, new Day05() },
-    // { 6, new Day06() },
+    { 6, new Day06() },
     // { 7, new Day07() },
     // { 8, new Day08() },
     // { 9, new Day09() },
